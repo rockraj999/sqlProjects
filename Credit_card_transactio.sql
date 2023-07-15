8564987dbc93cdaf2518662e9fc0f61968d4b9e0
Biswanath kumar prasad <121096737+rockraj999@users.noreply.github.com>
2023-07-15 18:43:37 +0530

    Credit_card_transactio.sql

--- Credit_card_transactio.sql
+++ Credit_card_transactio.sql
@@ -1,3 +1,20 @@
+----==========================================Customer Transaction Analysis=========================================-----------------
+
+--  Introduction
+--  In this project, I am going to analyze credit card transactions done by customers in India. for this, I fetched the data from Kaggle.
+--  The purpose of this project is to utilize my SQL skill in playing with data and finding insight that can be used to make Business 
+--  decision.
+
+
+--  Data Exploration
+--  In this dataset, it contains 26K+ rows and there are 7 columns: transaction_id, city, transaction_date, card_type, exp_type, gender
+--  and amount. here i ensured the data quality by confirming whether Null is present or not and same fro duplicate rows. this data set
+--  has transaction_id as its primary key. this dataset contains the transaction data from 2013-10 to 2015-05 done via credit card in
+--  India. In the card_type column, we have considered Silver, Signature, Gold and Platinum. and expense type(exp_type) column holds 
+--  values like Entertainment, Food, Bills, Fuel, Travel and Grocery.
+
+
+
 -- total 26052 transaction
 -- transaction_id(unique for each row), city, transaction_date, amount, card_type, exp_type, gender
 -- this dataset contains transaction data from 2013-10 to 2015-05
